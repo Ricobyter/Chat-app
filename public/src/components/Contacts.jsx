2d436eaf40d179a685cb2709bbc1d4571e354071
Dhruv <dhruvanandsingh2004@gmail.com>
2024-01-18 23:38:01 +0530

    added content in Contacts

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -1,8 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import Logo from '../assets/logo.svg' 
 
 function Contacts({contacts, currentUser}) {
+
+  const[currentUserName, setCurrentUserName] = useState(undefined)
+  const[currentUserImage, setCurrentUserImage] = useState(undefined)
+  const[currentSelected, setCurrentSelected] = useState(undefined)
+
+  useEffect(() =>{
+    if(currentUser){
+      setCurrentUserImage(currentUser.avatarImage);
+      setCurrentUserName(currentUser.username)
+    }
+  },[currentUser]);
+
+  const changeCurrentChat = (index,contact) =>{
+    
+  }
+
   return (
     <div>
       Contacts
