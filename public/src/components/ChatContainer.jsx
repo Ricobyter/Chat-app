d38b9e20a8e392e449d2fec07673800bef16876b
Dhruv <dhruvanandsingh2004@gmail.com>
2024-01-24 01:26:30 +0530

    added styling in ChatInput and added messages section

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logout from './Logout'
+import ChatInput from './ChatInput'
+import Messages from './Messages'
 
 export default function ChatContainer({currentChat}) {
+
+    const handleSendMsg = async (msg) => {}
+
   return (
     <>
     {
@@ -26,12 +31,8 @@ export default function ChatContainer({currentChat}) {
             </div>
             <Logout/>
         </div>
-        <div className="chat-messages">
-
-        </div>
-        <div className="chat-input">
-
-        </div>
+<Messages />
+<ChatInput handleSendMsg = {handleSendMsg}/>
     </Container>
         )}
     </>
---
+++ public/src/components/ChatInput.jsx
@@ -0,0 +1,91 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Picker from 'emoji-picker-react';
+import {IoMdSend} from 'react-icons/io'
+import {BsEmojiSmileFill} from 'react-icons/bs'
+
+export default function ChatInput() {
+  return (
+    <Container>
+      <div className="button-container">
+        <div className="emoji">
+            <BsEmojiSmileFill />
+        </div>
+      </div>
+
+      <form className='input-container'>
+        <input type="text" placeholder='Your message...'/>
+        <button className="submit">
+            <IoMdSend />
+        </button>
+      </form>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+display: grid;
+grid-template-columns: 5% 95%;
+align-items: center;
+background-color: #080420;
+padding: 0 2rem;
+padding-bottom: 0.3rem;
+.button-container{
+    display: flex;
+    align-items: center;
+    color: white;
+    gap: 1rem;
+    .emoji{
+       position: relative;
+        svg {
+           font-size: 2rem;
+           color: #ffff00c8;
+           cursor: pointer;
+        }
+    }
+}
+
+.input-container{
+    width: 100%;
+    border-radius: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    background-color: #ffffff34;
+
+    input{
+        width: 90%;
+        height: 60%; 
+        background-color: transparent;
+        color: white;
+        border: none;
+        padding-left: 1.2rem;
+        font-size: 1.2rem;
+        padding: 0.3rem;
+
+        &::selection{
+            background-color: #9186f3;
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+    button{
+        padding: 0.3rem 2rem;
+        border-radius: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #9a86f3;
+        border: none;
+        svg{
+            font-size: 2rem;
+            color: white;
+        }
+        
+        
+    }
+
+}
+
+`
